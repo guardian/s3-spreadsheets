63c3750db70730b8cb0653ee99cf95e7e5983752
andymason <andrew.mason@guardian.co.uk>
2014-10-03 12:55:42 +0100

    Removed cron for simple timeout and added status test file

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var async = require('async');
 var gSpreadsheet = require('./googleSpreadsheet');
 var s3 = require('./s3');
-var CronJob = require('cron').CronJob;
+var fs = require('fs');
 
 try {
   var config = require('./config.json');
 } catch(err) {
   console.log('Missing config.json. Use sample-config.json as a reference.');
 }
+
+var statusFile = './status.txt';
+var statusText;
+var FETCH_DELEY = 60 * 1000; // Wait for one minute
+var startTime;
+var endTime;
 
 /**
  * Handle the returned master spreadsheet data
@@ -16,8 +22,10 @@ try {
  * @param tabletop {object} - An instance of tabletop
  */
 function parseMastersheet(data, tabletop) {
-  var spreadsheets = tabletop.sheets('data').all();
-  async.map(spreadsheets, fetchSheet, parseSheets); 
+    // TODO: Handle mastersheet parse error
+    var spreadsheets = tabletop.sheets('data').all();
+    updateStatus('Got mastersheet. Spreadsheet count = ' + spreadsheets.length);
+    async.map(spreadsheets, fetchSheet, parseSheets); 
 }
 
 /**
@@ -27,23 +35,18 @@ function parseMastersheet(data, tabletop) {
  * @param sheets {array} - All the spreadsheets listed in the master file
  */
 function parseSheets(err, sheets) {
-  if (err) return console.error(err);
+    if (err) return console.error(err);
 
-  // Filter out null sheets
-  sheets = sheets.filter(function(sheet) { return sheet !== null; });
+    // Filter out null sheets
+    sheets = sheets.filter(function(sheet) { return sheet !== null; });
   
-  // Create JSON files
-  var uploadData = sheets.map(function(sheet) {
-    return createUploadData(sheet, false);
-  });
+    // Create JSON files
+    var uploadData = sheets.map(function(sheet) {
+        return createUploadData(sheet, false);
+    });
 
-  // Append JSONP files
-  uploadData = uploadData.concat(sheets.map(function(sheet) {
-    return createUploadData(sheet, true);
-  }));
-
-  // Upload all files
-  uploadSheets(uploadData);
+    // Upload all files
+    uploadSheets(uploadData);
 }
 
 /**
@@ -52,9 +55,7 @@ function parseSheets(err, sheets) {
  * @param sheets {array} - Sheets to be uploaded to S3
  */
 function uploadSheets(sheets) {
-  async.each(sheets, uploadSheet, function(err) {
-    if (err) return console.error(err);
-  });
+    async.each(sheets, uploadSheet, finished);
 }
 
 /**
@@ -67,7 +68,8 @@ function uploadSheets(sheets) {
  * @param callback {function} - Async callback on completion
  */
 function uploadSheet(data, callback) {
-  s3.put(data, callback);
+    updateStatus('Uploading sheet - ' + data.filename);
+    s3.put(data, callback);
 }
 
 /**
@@ -79,23 +81,19 @@ function uploadSheet(data, callback) {
  * @param data.sheet.cacheage {string} - Cache age
  * @param data.sheet.key {string} - Google spreadsheet key
  * @param data.tabletop {object} - Tabletop instance
- * @param isJSONP {boolean} - Wrap the JSON in a JSONP or not
  */
-function createUploadData(data, isJSONP) {
-  var spreadsheetName = (data.sheet.name) ? data.sheet.name : 'undefined';
-  var sheets = (data.sheet.specificsheets === "") ? [] : data.sheet.specificsheets.split(',');
-  var cacheControl = 'max-age=' + (data.sheet.cacheage || '60') + ', public';
-  var body = createJSON(data.tabletop, spreadsheetName, sheets);
-  if (isJSONP) {
-    body = createJSONP(body);
-  }
+function createUploadData(data) {
+    var spreadsheetName = (data.sheet.name) ? data.sheet.name : 'undefined';
+    var sheets = (data.sheet.specificsheets === "") ? [] : data.sheet.specificsheets.split(',');
+    var cacheControl = 'max-age=' + (data.sheet.cacheage || '60') + ', public';
+    var body = createJSON(data.tabletop, spreadsheetName, sheets);
 
-  return {
-    body: body,
-    filename: data.sheet.key + ((isJSONP) ? '.jsonp' : '.json'),
-    cacheControl: cacheControl,
-    contentType: (isJSONP) ? 'application/javascript' : 'application/json'
-  };
+    return {
+        body: body,
+        filename: data.sheet.key + '.json',
+        cacheControl: cacheControl,
+        contentType: 'application/json'
+    };
   
 }
 
@@ -108,40 +106,29 @@ function createUploadData(data, isJSONP) {
  * @returns {object} - Data for uploading containing JSON string
  */
 function createJSON(tabletop, spreadsheetName, sheets) {
-  var jsonContent = {
-      sheets: {},
-      updated: Date(),
-      name: spreadsheetName
-  };
+    var jsonContent = {
+        sheets: {},
+        updated: Date(),
+        name: spreadsheetName
+    };
 
-  // Return only specific sheets or all sheets
-  if (sheets.length > 0) {
-    sheets.forEach(function(sheetName) {
-        var sheet = sheetName.trim();
-        if (tabletop.model_names.indexOf(sheet) > -1) {
-            jsonContent.sheets[sheet] = tabletop.sheets(sheet).all();
-        }
-    });
-  } else {
-      tabletop.model_names.forEach(function(modelName) {
-          jsonContent.sheets[modelName] = tabletop.sheets(modelName).all();
-      });
-  }
+    // Return only specific sheets or all sheets
+    if (sheets.length > 0) {
+        sheets.forEach(function(sheetName) {
+            var sheet = sheetName.trim();
+            if (tabletop.model_names.indexOf(sheet) > -1) {
+                jsonContent.sheets[sheet] = tabletop.sheets(sheet).all();
+            }
+        });
+    } else {
+        tabletop.model_names.forEach(function(modelName) {
+            jsonContent.sheets[modelName] = tabletop.sheets(modelName).all();
+        });
+    }
 
-  var json = JSON.stringify(jsonContent);
-  json = json.replace(/(\r\n|\n|\r)/gm, '');
-  return json;
-}
-
-
-/**
- * Wrap JSON string in a function call creating a JSONP string
- *
- * @param json {string} - JSON string to wrap
- * @returns {string} - JSONP string
- */
-function createJSONP(json) {
-  return config.callbackName + '(' + json + ');';
+    var json = JSON.stringify(jsonContent);
+    json = json.replace(/(\r\n|\n|\r)/gm, '');
+    return json;
 }
 
 
@@ -152,7 +139,7 @@ function createJSONP(json) {
  * @returns {boolean} - Valid status
  */
 function isValidKey(key) {
-  return (key.length === 44 && (true === /^[\d\w-]+$/.test(key)));
+    return (key.length === 44 && (true === /^[\d\w-]+$/.test(key)));
 }
 
 
@@ -164,36 +151,77 @@ function isValidKey(key) {
  * @param callback {function} - Async callback on completion
  */
 function fetchSheet(sheet, callback) {
-  // Check sheet has a valid key
-  if (isValidKey(sheet.key) === false) {
-    console.log('Spreadsheet key is invalid: ' + sheet.key);
-    return callback(null, null);
-  }
+    // Check sheet has a valid key
+    if (isValidKey(sheet.key) === false) {
+        console.log('Spreadsheet key is invalid: ' + sheet.key);
+        updateStatus('ERROR - Sheet key is invalid - ' + sheet.key);
+        return callback(null, null);
+    }
 
-  if (!sheet.valid || sheet.valid !== "TRUE") {
-    console.log('Spreadsheet valid status is not "TRUE": ' + sheet.key);
-    return callback(null, null);
-  }
+    if (!sheet.valid || sheet.valid !== "TRUE") {
+        console.log('Spreadsheet valid status is not "TRUE": ' + sheet.key);
+        updateStatus('ERROR - Sheet valid status != "TRUE" - ' + sheet.key);
+        return callback(null, null);
+    }
 
-  gSpreadsheet.fetch(sheet.key, function(data, tabletop) {
-    callback(null, {sheet: sheet, tabletop: tabletop});
-  });
+    gSpreadsheet.fetch(sheet.key, function(data, tabletop) {
+        updateStatus('Fetched - ' + sheet.key);
+        callback(null, {sheet: sheet, tabletop: tabletop});
+    });
 }
 
 
-/**
- * Action to run on every cron tick
- */
-function cronTickAction() {
+
+function finished(err) {
+    if (err) {
+        console.error(Date() + 'Error: ', err);
+    }
+    
+    var endTime = new Date();
+    var timeTaken = endTime - startTime;
+
+    // Log warning if process it taking too long
+    if (timeTaken >= FETCH_DELEY) {
+        console.log(Date() +' WARNING: Process taking a long time: ' + timeTaken);
+        updateStatus('WARNING: Process taking a long time: ' + timeTaken);
+    }
+
+    var delay = FETCH_DELEY - timeTaken;
+    updateStatus('Next fetch in ' + delay / 1000 + 's');
+
+    // Check if last fetch took longer than delay and force instance fetch
+    if (delay < 0) {
+        start();
+    } else {
+        setTimeout(start, delay); 
+    }
+
+    updateStatus('Finished in ' + timeTaken / 1000 + 's');
+    outputStatusFile();
+}
+
+
+function updateStatus(msg) {
+    statusText += Date() + ': ' + msg + '\n';
+}
+
+function outputStatusFile() {
+    fs.writeFile(statusFile, statusText, function(err) {
+        if (err) {
+            console.log('ERROR writing status file', err);
+        }
+    });
+}
+
+function start() {
+    startTime = new Date();
+    
+    // Create new status text string
+    statusText = '';
+    updateStatus('Starting fetch');
+
+    // TODO: Handle master sheet fetch fail
     gSpreadsheet.fetch(config.masterKey, parseMastersheet);
 }
 
-// Start a schedualed fetch and upload of all spreadsheets
-// Once every minute
-var cronJob = new CronJob({
-  cronTime: '00 * * * * *',
-  onTick: cronTickAction,
-  start: false
-});
-
-cronJob.start();
+start();
